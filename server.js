e7c4600eb447df2e56e254adaa1753168e168aa7
aaamg <andrewmanuelgarcia@gmail.com>
2019-10-30 13:45:45 -0400

    update

--- server.js
+++ server.js
@@ -17,8 +17,19 @@ function dateLogger(req, res, next) {
 }
 
 function logger(req, res, next){
-  
+
 }
+
+function gateKeeper(req, res, next) {
+  // data can come in the body, url parameters, query string, headers
+  // new way of reading data sent by the client
+  const password = req.headers.password || '';
+  if (password.toLowerCase() === 'mellon') {
+    next();
+  } else {
+    res.status(400).json({ you: 'cannot pass!!' });
+  }
+
 
 server.use('/api/hubs', hubsRouter);
 
